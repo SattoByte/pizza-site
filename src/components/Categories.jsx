cb2a125d85a6db1d54c64fb378832ae78de8aae2
SattoByte <sattobyte@gmail.com>
2024-07-31 17:09:06 -0400

    added couple components for start

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">All</li>
+        <li>With Meat</li>
+        <li>Vegeterian</li>
+        <li>Grill</li>
+        <li>Spicy</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Categories
